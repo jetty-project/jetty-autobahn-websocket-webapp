bd65d7a40a1e7684b4e4058e151e169b5c5bb549
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2011-11-03 15:18:56 -0700

    Bumping up to 7.5.5-SNAPSHOT

--- src/test/java/org/eclipse/jetty/test/TestServer.java
+++ src/test/java/org/eclipse/jetty/test/TestServer.java
@@ -16,11 +16,16 @@ package org.eclipse.jetty.test;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.log.StdErrLog;
 
 public class TestServer
 {
     public static void main(String[] args) throws Exception
     {
+        System.setProperty("org.eclipse.jetty.util.log.class",StdErrLog.class.getName());
+        System.setProperty("org.eclipse.jetty.websocket.STRICT","true");
+        System.setProperty("org.eclipse.jetty.LEVEL","DEBUG");
+        
         Server server = new Server(8080);
 
         ServletContextHandler context = new ServletContextHandler();
